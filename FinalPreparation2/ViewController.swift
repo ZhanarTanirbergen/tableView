cba2678fb51e6bac0fdac8b521c187bc0b7a61af
Zhanara Tanirbergenova <t-zhanar-a@mail.ru>
2017-12-06 07:44:44 +0600

    Initial Commit

---
+++ FinalPreparation2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FinalPreparation2
+//
+//  Created by Zhanara Tanirbergenova on 06.12.17.
+//  Copyright © 2017 Zhanara Tanirbergenova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
